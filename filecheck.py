fdc820d3b3a74e34fa59beffb567e5c59c2059be
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2025-05-27 23:30:46 -0700

    ran out of time, have to sleep

--- filecheck.py
+++ filecheck.py
@@ -1,4 +1,21 @@
 import os
 import re
 
-directory = 
+directory = '/home/helloxonie/code/pythonfilecheck/env' //change to your own directory
+prefix = 'file'
+
+expected_numbers = set(range(1,6))
+pattern = re.compile(rf'{re.escape(prefix)}(\d+)')
+
+found_numbers = set()
+
+for filename in os.listdir('/home/helloxonie/code/pythonfilecheck/env'):
+    match = pattern.match(filename)
+    if match:
+        number = int (match.group(1))
+        found_numbers.add(number)
+
+missing_numbers = expected_numbers - found_numbers
+if missing_numbers:
+    print 
+
